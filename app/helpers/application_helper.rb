363f6546455b4970e217b849ceb1ecd226d22f1a
Tony Ennis <ennis.tony@gmail.com>
2023-03-31 14:47:14 +0000

    Add some new stuff to the default codebase. Mainly structure related

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def page_title
+      "Site Title"
+    end
+
+    def user_signed_in?
+      # This is a stub, which should be removed when authentication is implemented
+      false
+    end
+
 end
