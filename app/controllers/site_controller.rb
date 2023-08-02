788be4c3acd861e9f3477ee83e6797081060171b
tonyennis145 <ennis.tony@gmail.com>
2023-08-02 21:57:44 +0100

    Several changes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,13 +1,46 @@
 class SiteController < ApplicationController
 
-  before_action :allow_iframe
+  # TODO: Remove this. Coding offline and can't remember one-liner to include the auth token in the form
+  skip_before_action :verify_authenticity_token, :only => [:plain_remote_form, :remote_form]
 
   def index 
     @content = Utilities.markdown_to_html("readme.md")
   end
 
-  def allow_iframe
-    response.headers["X-FRAME-OPTIONS"] = "ALLOWALL"
+  def remote_content
+    support_partial_response
+  end
+
+  def deferred_content
+    support_partial_response
+  end
+
+  def dropdown_content
+    support_partial_response
+  end
+
+  def toast_content
+    support_partial_response
+  end
+
+  def plain_remote_form 
+    render partial: "partials/plain_remote_form", layout: false
+  end
+
+  def remote_form 
+    if request.post?
+      @user = User.new(user_params)
+      # For the purposes of demonstration, set a password here to allow persistence.
+      pw = SecureRandom.hex(10)
+      @user.password = pw 
+      @user.password_confirmation = pw 
+      @user.save
+    end
+    render partial: "partials/remote_form", layout: false
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:first_name,:last_name)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   root to: "site#index"
 
+  get "/examples" => "site#examples", as: "examples"
+
   get "modal-content" => "site#modal_content", as: "modal_content"
+  get "remote-content" => "site#remote_content", as: "remote_content"
+  get "deferred-content" => "site#deferred_content", as: "deferred_content"
+  get "dropdown-content" => "site#dropdown_content", as: "dropdown_content"
+  get "toast-content" => "site#toast_content", as: "toast_content"
+
+  match "/plain_remote_form" => "site#plain_remote_form", as: "plain_remote_form", via: [:get,:post]
+  match "/remote_form" => "site#remote_form", as: "remote_form", via: [:get,:post]
+
 end
