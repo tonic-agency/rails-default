a892c1dce02877515ead0098cd2b8b3b138cef73
Megan Ennis <mennis0145@gmail.com>
2023-12-05 09:08:32 +0100

    Remove redundant OTP test view

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -3,20 +3,6 @@ class WebsiteController < ApplicationController
   # TODO: Remove this. Coding offline and can't remember one-liner to include the auth token in the form
   skip_before_action :verify_authenticity_token, :only => [:plain_remote_form, :remote_form]
 
-  def test_otp 
-    @kyc_onboarding = current_user.kyc_onboarding
-    if request.post?
-      otp_array = params.values_at(:otp_1, :otp_2, :otp_3, :otp_4, :otp_5, :otp_6)
-
-      passcode = otp_array.join
-      if @kyc_onboarding.mobile_otp.validate_otp(passcode)
-        flash[:notice] = "OTP validated"
-      else
-        flash[:alert] = "OTP invalid"
-      end
-    end
-  end
-  
   def show_docs_page 
     params[:file].present? ? @filename = "docs/#{params[:file]}.md" : @filename = "readme.md"
     @content = Utilities.markdown_to_html("#{@filename}")
