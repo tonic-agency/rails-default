3717cd988c547ce8a3d646c717585e2ede713b46
Megan Ennis <mennis0145@gmail.com>
2023-12-14 12:43:37 +0100

    Allow for disabling 2FA functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def feature_flag?(env_key)
+    return false if env_key.blank?
+    
+    # returns whether value is true or false
+    return ENV.fetch(env_key, false).to_s.casecmp('true').zero?
+  end
+
+  helper_method :feature_flag?
+
   def authenticate_user_otp!
+    return unless feature_flag?('2FA_ENABLED')
     return unless current_user.login_otp.present?
     
     if current_user.login_otp.validated_at.nil?
@@ -19,11 +29,12 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    if resource.is_a?(User)
+    if resource.is_a?(User) && feature_flag?('2FA_ENABLED')
       return validate_user_otp_path if resource.login_otp.present? && resource.login_otp.validated_at.nil?
+    else 
+      return app_home_path
     end 
-    
-    super
+
   end
   
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,16 +3,16 @@ class Users::SessionsController < Devise::SessionsController
   
   def create
     super do |user|
-      return unless user&.login_otp.present?
-      return unless user&.kyc_onboarding&.phone.present?
-      return unless user&.kyc_onboarding&.state == KycOnboarding::STATES[:submitted][:id]
+      return redirect_to app_home_path if user&.kyc_onboarding&.state == KycOnboarding::STATES[:submitted][:id]
       
       if user.persisted?
-        @otp = user.login_otp
-        @otp.generate_otp!
-        @otp.mobile = user.kyc_onboarding.phone
-        message = "Enter the one-time-password (OTP) below to log in to Farmbank.\n\n #{@otp.value} \n\nFor your account safety please do not share this with anyone."
-        @otp.send_sms(message)
+        if feature_flag?('2FA_ENABLED') && user&.login_otp.present? && user&.kyc_onboarding&.phone.present?
+          @otp = user.login_otp
+          @otp.generate_otp!
+          @otp.mobile = user.kyc_onboarding.phone
+          message = "Enter the one-time-password (OTP) below to log in to Farmbank.\n\n #{@otp.value} \n\nFor your account safety please do not share this with anyone."
+          @otp.send_sms(message)
+        end
       end
     end
   end
